4393d72354404e00d5e4be5d3fb1efc09c1eb699
Rogerio Amorim <rogeriol@gmail.com>
2024-01-17 11:52:56 -0300

    implements function to send notification to user

---
+++ neogpt/utils/notify.py
@@ -0,0 +1,20 @@
+import asyncio
+from desktop_notifier import DesktopNotifier
+
+notifier = DesktopNotifier()
+
+async def notify(title, message, timeout=10):
+    """
+    Show a desktop notification to user
+
+    Parameter:
+    title: notification title - required
+    message: notification message - required
+    timeout:  timeout in seconds to hide the notification. default value is 10 - optional 
+    """
+    n = await notifier.send(title=title, message=message)
+    await asyncio.sleep(timeout)  
+    await notifier.clear(n)  
+
+## test 
+## asyncio.run(notify('test', 'this is a test notification'))
