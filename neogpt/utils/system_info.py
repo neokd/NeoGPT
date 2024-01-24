56a3cde5d9a18cf70508614e63b7bd7a82744977
Kuldeep <kuldeep@getmastery.ai>
2024-01-24 11:04:14 +0530

    updated utils for new functions

---
+++ neogpt/utils/system_info.py
@@ -0,0 +1,6 @@
+# def get_python_version() to return the currect python version
+# def get_os_version() to return the OS the system is using
+# def get_ram_info() to get the Free/Used RAM capacity
+# def get_cpu_info() to check the resources utlised
+# def get_neogpt_version() to check the current version of NeoGPT
+# def neogpt_info() to display current version of neogpt, model used, max_tokens, 
---
+++ neogpt/utils/user_info.py
@@ -0,0 +1,2 @@
+# def get_username() to get the current user name 
+# def get_user_info() to return the current user name ,  shell name, os name, current working directory.
