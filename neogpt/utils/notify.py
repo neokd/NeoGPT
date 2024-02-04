2bba7e44f2786903b26975088f05082a75a78b0a
neokd <neokd@protonmail.com>
2024-02-05 00:28:30 +0530

    updated with together AI and docs

--- neogpt/utils/notify.py
+++ neogpt/utils/notify.py
@@ -1,7 +1,17 @@
 import asyncio
+
 from plyer import notification
 
-async def notify(title, message, timeout=10, ticker = 'NeoGPT says Hi!', app_name='NeoGPT', app_icon="docs/assets/icon.ico",toast=False):
+
+async def notify(
+    title,
+    message,
+    timeout=10,
+    ticker="NeoGPT says Hi!",
+    app_name="NeoGPT",
+    app_icon="docs/assets/icon.ico",
+    toast=False,
+):
     """
     Show a desktop notification to user
 
@@ -12,21 +22,22 @@ async def notify(title, message, timeout=10, ticker = 'NeoGPT says Hi!', app_nam
     app_name (str): name of the app - optional
     app_icon (str): path to the icon of the app - optional
 
-    The function uses the plyer library to send desktop notifications. Plyer is a Python library 
+    The function uses the plyer library to send desktop notifications. Plyer is a Python library
     for accessing features of your hardware / platforms.
     """
     # Send a notification with the given title and message
     notification.notify(
-        title=title, 
-        message=message, 
-        app_name=app_name, 
-        app_icon=app_icon, 
+        title=title,
+        message=message,
+        app_name=app_name,
+        app_icon=app_icon,
         timeout=timeout,
         toast=toast,
-        #ticker=ticker
+        # ticker=ticker
     )
     # Sleep for the duration of the timeout, then continue
     await asyncio.sleep(timeout)
+
 
 # Test the notify function
 # asyncio.run(notify('test', 'this is a test notification'))
